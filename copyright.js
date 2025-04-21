24da45491252cfecb0a27502b0f1d4ca685dabeb
itoh <yuki@frontartgraph.com>
2025-04-21 15:38:36 +0900

    Changed to 20250421 15:38:36

--- copyright.js
+++ copyright.js
@@ -1,19 +1,22 @@
-const copyrightSet = () => {
-  console.clear()
-  const styles = `font-size: 16px;
-      line-height: 1.4;
-      padding-inline: 20px;
-      color: #fff;
-      background: #020016;
-    `
-  console.log(
-    '%c' +
-      'Created by WALKALONE : https://walkal.one \nTwitter : https://twitter.com/___walkalone',
-    styles
-  )
-}
-copyrightSet()
-
-setTimeout(() => {
+// httpsならreturn
+if (location.protocol === 'https:') {
+  const copyrightSet = () => {
+    console.clear()
+    const styles = `font-size: 16px;
+        line-height: 1.4;
+        padding-inline: 20px;
+        color: #fff;
+        background: #020016;
+      `
+    console.log(
+      '%c' +
+        'Created by WALKALONE : https://walkal.one \nTwitter : https://twitter.com/___walkalone',
+      styles
+    )
+  }
   copyrightSet()
-}, 2000)
+
+  setTimeout(() => {
+    copyrightSet()
+  }, 2000)
+}
